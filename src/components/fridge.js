37ef4238aff0e2d54299b0838d93f48f8405800d
Alfred <castellum.a@gmail.com>
2021-03-23 12:51:16 +0100

    Added id property to fridge ingredients

--- src/components/fridge.js
+++ src/components/fridge.js
@@ -16,34 +16,42 @@ export let fridge = {
         {
           value: "Beef",
           label: "Beef",
+          id: "1",
         },
         {
           value: "Chicken",
           label: "Chicken",
+          id: "2",
         },
         {
           value: "Lamb",
           label: "Lamb",
+          id: "3",
         },
         {
           value: "Turkey",
           label: "Turkey",
+          id: "4",
         },
         {
           value: "Pork",
           label: "Pork",
+          id: "5",
         },
         {
           value: "Duck",
           label: "Duck",
+          id: "6",
         },
         {
           value: "Veal",
           label: "Veal",
+          id: "7",
         },
         {
           value: "Ham",
           label: "Ham",
+          id: "8",
         },
       ],
     },
@@ -54,58 +62,72 @@ export let fridge = {
         {
           value: "Salmon",
           label: "Salmon",
+          id: "9",
         },
         {
           value: "Tuna",
           label: "Tuna",
+          id: "10",
         },
         {
           value: "Cod",
           label: "Cod",
+          id: "11",
         },
         {
           value: "Haddock",
           label: "Haddock",
+          id: "12",
         },
         {
           value: "Hake",
           label: "Hake",
+          id: "13",
         },
         {
           value: "Herring",
           label: "Herring",
+          id: "14",
         },
         {
           value: "Mackerel",
           label: "Mackerel",
+          id: "15",
         },
         {
           value: "Whiting",
           label: "Whiting",
+          id: "16",
         },
         {
           value: "Squid",
           label: "Squid",
+          id: "17",
         },
         {
           value: "Sardine",
           label: "Sardine",
+          id: "18",
         },
         {
           value: "Crab",
           label: "Crab",
+          id: "19",
         },
         {
           value: "Octopus",
           label: "Octopus",
+          id: "20",
         },
         {
           value: "Lobster",
           label: "Lobster",
+          id: "21",
         },
         {
           value: "Snails",
           label: "Snails",
+          id: "22",
         },
       ],
     },
@@ -116,90 +138,112 @@ export let fridge = {
         {
           value: "Tomato",
           label: "Tomato",
+          id: "23",
         },
         {
           value: "Courgette",
           label: "Courgette",
+          id: "24",
         },
         {
           value: "Leeks",
           label: "Leeks",
+          id: "25",
         },
         {
           value: "Peppers",
           label: "Peppers",
+          id: "26",
         },
         {
           value: "Jalapeños",
           label: "Jalapeños",
+          id: "27",
         },
         {
           value: "Broccoli",
           label: "Broccoli",
+          id: "28",
         },
         {
           value: "Cauliflower",
           label: "Cauliflower",
+          id: "29",
         },
         {
           value: "Cucumber",
           label: "Cucumber",
+          id: "30",
         },
         {
           value: "Eggplant",
           label: "Eggplant",
+          id: "31",
         },
         {
           value: "Pumpkin",
           label: "Pumpkin",
+          id: "32",
         },
         {
           value: "Potato",
           label: "Potato",
+          id: "33",
         },
         {
           value: "Mushrooms",
           label: "Mushrooms",
+          id: "34",
         },
         {
           value: "Brussels sprouts",
           label: "Brussels sprouts",
+          id: "35",
         },
         {
           value: "Carrot",
           label: "Carrot",
+          id: "36",
         },
         {
           value: "Beetroot",
           label: "Beetroot",
+          id: "37",
         },
         {
           value: "Spinach",
           label: "Spinach",
+          id: "38",
         },
         {
           value: "Lettuce",
           label: "Lettuce",
+          id: "39",
         },
         {
           value: "Sweet potato",
           label: "Sweet potato",
+          id: "40",
         },
         {
           value: "Sweet corn",
           label: "Sweet corn",
+          id: "41",
         },
         {
           value: "Beans",
           label: "Beans",
+          id: "42",
         },
         {
           value: "Asparagus",
           label: "Asparagus",
+          id: "43",
         },
         {
           value: "Bellpepper",
           label: "Bellpepper",
+          id: "44",
         },
       ],
     },
@@ -210,82 +254,102 @@ export let fridge = {
         {
           value: "Banana",
           label: "Banana",
+          id: "45",
         },
         {
           value: "Apple",
           label: "Apple",
+          id: "46",
         },
         {
           value: "Orange",
           label: "Orange",
+          id: "47",
         },
         {
           value: "Cherry",
           label: "Cherry",
+          id: "48",
         },
         {
           value: "Plum",
           label: "Plum",
+          id: "49",
         },
         {
           value: "Apricot",
           label: "Apricot",
+          id: "50",
         },
         {
           value: "Pear",
           label: "Pear",
+          id: "51",
         },
         {
           value: "Peach",
           label: "Peach",
+          id: "52",
         },
         {
           value: "Watermelon",
           label: "Watermelon",
+          id: "53",
         },
         {
           value: "Lemon",
           label: "Lemon",
+          id: "54",
         },
         {
           value: "Lime",
           label: "Lime",
+          id: "55",
         },
         {
           value: "Grapefruit",
           label: "Grapefruit",
+          id: "56",
         },
         {
           value: "Mango",
           label: "Mango",
+          id: "57",
         },
         {
           value: "Lychee",
           label: "Lychee",
+          id: "58",
         },
         {
           value: "Passion fruit",
           label: "Passion fruit",
+          id: "59",
         },
         {
           value: "Strawberry",
           label: "Strawberry",
+          id: "60",
         },
         {
           value: "Raspberry",
           label: "Raspberry",
+          id: "61",
         },
         {
           value: "Pineapple",
           label: "Pineapple",
+          id: "62",
         },
         {
           value: "Kiwi",
           label: "Kiwi",
+          id: "63",
         },
         {
           value: "Coconut",
           label: "Coconut",
+          id: "64",
         },
       ],
     },
@@ -296,30 +360,37 @@ export let fridge = {
         {
           value: "Butter",
           label: "Butter",
+          id: "65",
         },
         {
           value: "Milk",
           label: "Milk",
+          id: "66",
         },
         {
           value: "Cream",
           label: "Cream",
+          id: "67",
         },
         {
           value: "Yogurt",
           label: "Yogurt",
+          id: "68",
         },
         {
           value: "Cheese",
           label: "Cheese",
+          id: "69",
         },
         {
           value: "Ice cream",
           label: "Ice cream",
+          id: "70",
         },
         {
           value: "Eggs",
           label: "Eggs",
+          id: "71",
         },
       ],
     },
@@ -330,34 +401,42 @@ export let fridge = {
         {
           value: "Pasta",
           label: "Pasta",
+          id: "72",
         },
         {
           value: "Rice",
           label: "Rice",
+          id: "73",
         },
         {
           value: "Couscous",
           label: "Couscous",
+          id: "74",
         },
         {
           value: "Flour",
           label: "Flour",
+          id: "75",
         },
         {
           value: "Yeast",
           label: "Yeast",
+          id: "76",
         },
         {
           value: "Bread",
           label: "Bread",
+          id: "77",
         },
         {
           value: "Tortilla",
           label: "Tortilla",
+          id: "78",
         },
         {
           value: "Sugar",
           label: "Sugar",
+          id: "79",
         },
       ],
     },
@@ -368,74 +447,92 @@ export let fridge = {
         {
           value: "Salt",
           label: "Salt",
+          id: "80",
         },
         {
           value: "Pepper",
           label: "Pepper",
+          id: "81",
         },
         {
           value: "Garlic",
           label: "Garlic",
+          id: "82",
         },
         {
           value: "Onion",
           label: "Onion",
+          id: "83",
         },
         {
           value: "Rosemary",
           label: "Rosemary",
+          id: "84",
         },
         {
           value: "Parsley",
           label: "Parsley",
+          id: "85",
         },
         {
           value: "Ginger",
           label: "Ginger",
+          id: "86",
         },
         {
           value: "Mint",
           label: "Mint",
+          id: "87",
         },
         {
           value: "Chili peppers",
           label: "Chili peppers",
+          id: "88",
         },
         {
-          value: "Thym",
-          label: "Thym",
+          value: "Thyme",
+          label: "Thyme",
+          id: "89",
         },
         {
           value: "Vinegar",
           label: "Vinegar",
+          id: "89",
         },
         {
           value: "Mustard",
           label: "Mustard",
+          id: "90",
         },
         {
           value: "Basil",
-          label: "basil",
+          label: "Basil",
+          id: "91",
         },
         {
           value: "Oregano",
           label: "Oregano",
+          id: "92",
         },
         {
           value: "Shallot",
           label: "Shallot",
+          id: "93",
         },
         {
           value: "Paprika",
           label: "Paprika",
+          id: "94",
         },
         {
           value: "Black Pepper",
           label: "Black Pepper",
+          id: "95",
         },
         {
           value: "White Pepper",
           label: "White Pepper",
+          id: "96",
         },
       ],
     },
@@ -446,54 +543,67 @@ export let fridge = {
         {
           value: "Mayonnaise",
           label: "Mayonnaise",
+          id: "97",
         },
         {
           value: "Ketchup",
           label: "Ketchup",
+          id: "98",
         },
         {
           value: "Tomato sauce",
           label: "Tomato sauce",
+          id: "99",
         },
         {
           value: "Aioli",
           label: "Aioli",
+          id: "100",
         },
         {
           value: "Bbq",
           label: "Bbq",
+          id: "101",
         },
         {
           value: "Pesto",
           label: "Pesto",
+          id: "102",
         },
         {
           value: "Soy sauce",
           label: "Soy sauce",
+          id: "103",
         },
         {
           value: "Tabasco",
           label: "Tabasco",
+          id: "104",
         },
         {
           value: "Oyster sauce",
           label: "Oyster sauce",
+          id: "105",
         },
         {
           value: "Salsa",
           label: "Salsa",
+          id: "106",
         },
         {
           value: "Harissa",
           label: "Harissa",
+          id: "107",
         },
         {
           value: "Thousand Island Dressing",
           label: "Thousand Island Dressing",
+          id: "108",
         },
         {
           value: "Sriracha",
           label: "Sriracha",
+          id: "109",
         },
       ],
     },
