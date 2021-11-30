7053a8e66cf748240a206e4f06569a208ed9873d
Alfred Castillo <a.castillo.staiy@gmail.com>
2021-11-30 15:33:00 +0100

    Assigned variable to default export of images in assets/img/index

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -17,23 +17,44 @@ import recipeResultBG from "./recipeResults.jpg";
 import sauce from "./sauceIcons.png";
 import vegetables from "./vegetablesIcon.png";
 
-export default { 
-    chefPika,
-    dairy,
-    fish,
-    fruits,
-    grain,
-    grilledGoodsBG,
-    herbsSpice,
-    animeHomeBG,
-    foodsHomeBG,
-    ingredientsBGMob,
-    ingredientsBGDesk,
-    loadingPika,
-    meat,
-    pikachuHome,
-    pokeBubble,
-    recipeResultBG,
-    sauce,
-    vegetables,
+const images = {
+  chefPika,
+  dairy,
+  fish,
+  fruits,
+  grain,
+  grilledGoodsBG,
+  herbsSpice,
+  animeHomeBG,
+  foodsHomeBG,
+  ingredientsBGMob,
+  ingredientsBGDesk,
+  loadingPika,
+  meat,
+  pikachuHome,
+  pokeBubble,
+  recipeResultBG,
+  sauce,
+  vegetables,
 };
+
+export default images;
+
+// export { default as chefPika } from "./chefpi.png";
+// export { default as dairy } from "./dairyIcon.png";
+// export { default as fish } from "./fishIcon.png";
+// export { default as fruits } from "./fruitsIcon.png";
+// export { default as grain } from "./grainIcon.png";
+// export { default as grilledGoodsBG } from "./grilledGoods.jpg";
+// export { default as herbsSpice } from "./herb&spiceIcon.png";
+// export { default as animeHomeBG } from "./homeBackground.gif";
+// export { default as foodsHomeBG } from "./homeBackground.jpg";
+// export { default as ingredientsBGMob } from "./ingredientsBg.jpg";
+// export { default as ingredientsBGDesk } from "./ingredientsDesk.jpg";
+// export { default as loadingPika } from "./loadingPika.gif";
+// export { default as meat } from "./meatIcon.png";
+// export { default as pikachuHome } from "./pikachuHome.gif";
+// export { default as pokeBubble } from "./pokebubblebox.png";
+// export { default as recipeResultBG } from "./recipeResults.jpg";
+// export { default as sauce } from "./sauceIcons.png";
+// export { default as vegetables } from "./vegetablesIcon.png";
